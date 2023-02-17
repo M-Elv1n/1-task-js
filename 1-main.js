1251f74248c4467cd05d1ba30677770f74c5858b
M-Elv1n <e.musayev590@gmail.com>
2023-02-17 21:22:48 +0400

    1-js tasks

---
+++ 1-main.js
@@ -0,0 +1,80 @@
+// 1. Input ile daxil olunan ədədlerin cemi.
+
+// let a = +prompt("nece eded daxil etmek isteyirsiniz?:")
+// let count = 0
+// let c = 0
+
+// for (let i = 0; i < a; i++) {
+//     c = c + +prompt(` indeksli edidi daxil edin`) 
+// }
+// console.log(c)
+
+// =======================================================
+
+// 2. Input ile daxil olunan musbet ədədlerin cemi. 
+
+// let a = +prompt("nece eded daxil etmek isteyirsiniz?:")
+// let count = 0
+// let c = 0
+
+// for (let i = 0; i < a; i++) {
+//     let d = +prompt(` ededi daxil edin`);
+//     if (d > 0) {
+//         c = c + d
+//     }
+// }
+// console.log(c)
+
+// ===========================================================
+
+// 3. Input ile daxil olunan ədədin sıfır,müsbət yoxsa mənfi olduğunu tapın.
+
+// let a = +prompt("nece eded daxil etmek isteyirsiniz?:")
+
+// if (a>0) {
+//     console.log("eded musbetdir")
+// }else if(a<0){
+//     console.log("eded menfidir")
+// }else{
+//     console.log("eded sifira beraberdir")
+// }
+
+// =======================================================
+
+// 4. Input ile daxil olunan 2 eded arasinda olan en boyuk ededi tapin
+
+// let a = +prompt("birinci ededi daxil et")
+
+// let b = +prompt("ikinci ededi daxil et")
+
+// if (a > b) {
+//     console.log(a)
+// } else {
+//     console.log(b)
+// }
+
+// =======================================================
+
+// 5. Diskriminantın tapılması
+
+// let a = +prompt("b-in ededin yazin")
+// let b = +prompt("a-in ededin yazin")
+// let c = +prompt("c-in ededin yazin")
+
+// console.log("Diskriminantın beraberdir: ", b**2 - 4*a*c)
+
+// let a = +prompt("regemleri daxil et")
+
+// switch (a){
+//     case 18:
+//         console.log("18")
+//         break
+//     case 17:
+//         console.log("17")
+//         break
+//     case 20:
+//         console.log("20")
+//         break
+//     default:
+//         console.log("any")
+// }
